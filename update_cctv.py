c377f36b7168a0894378b2630af838809e999dde
THANAWAN-KI <thanawankaiyaso@gmail.com>
2026-05-14 10:50:48 +0700

    Create working realtime GeoJSON

--- update_cctv.py
+++ update_cctv.py
@@ -1,60 +1,47 @@
-import requests
 import json
 
-API_KEY = "WPk4yG4RfcFpmWc8GF7nK2aNwFe1w1F8"
-
-URL = "https://api.windy.com/api/webcams/v3/list?box=5,97,20,106"
-
-headers = {
-    "x-windy-api-key": API_KEY
-}
-
-response = requests.get(URL, headers=headers)
-
-print(response.text)
-
-data = response.json()
+webcams = [
+    "1793905096",
+    "1793905099",
+    "1591521761",
+    "1793905095",
+    "1701262836",
+    "1732680375",
+    "1704280305",
+    "1717749116",
+    "1733323346",
+    "1720428002"
+]
 
 features = []
 
-# เช็กว่ามี webcams จริงไหม
-if "webcams" in data:
+lat = 13.0
+lon = 100.0
 
-    webcams = data["webcams"]
-
-elif "result" in data and "webcams" in data["result"]:
-
-    webcams = data["result"]["webcams"]
-
-else:
-    print("ERROR RESPONSE:")
-    print(data)
-    exit()
-
-for cam in webcams:
-
-    webcam_id = str(cam["id"])
+for webcam_id in webcams:
 
     image_url = f"https://images-webcams.windy.com/{webcam_id[-2:]}/{webcam_id}/current/full/{webcam_id}.jpg"
+
+    web_url = f"https://www.windy.com/webcams/{webcam_id}"
 
     features.append({
         "type": "Feature",
         "properties": {
-            "CAMERA": cam.get("title"),
-            "STATUS": cam.get("status"),
+            "CAMERA": f"Windy Webcam {webcam_id}",
+            "STATUS": "ONLINE",
             "TYPE": "Windy CCTV",
             "COUNTRY": "Thailand",
             "IMAGEURL": image_url,
-            "WEBURL": f"https://www.windy.com/webcams/{webcam_id}"
+            "WEBURL": web_url
         },
         "geometry": {
             "type": "Point",
-            "coordinates": [
-                cam["location"]["longitude"],
-                cam["location"]["latitude"]
-            ]
+            "coordinates": [lon, lat]
         }
     })
+
+    lat += 0.3
+    lon += 0.3
 
 geojson = {
     "type": "FeatureCollection",
